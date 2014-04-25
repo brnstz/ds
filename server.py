96cb6a082dc0de06a2c5aab31845a48eed24b080
Brian Seitz <brnstz@gmail.com>
2014-04-25 17:26:59 -0600

    starting to define cluster api format

--- server.py
+++ server.py
@@ -4,12 +4,16 @@ import pandas
 import os.path
 import h5py
 import pprint
+import decimal
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from sklearn.cluster import MiniBatchKMeans, KMeans
 
 LOCAL_ROOT="/mnt/msd/AdditionalFiles"
 MSD_ROOT="/mnt/msd/data"
 ANALYSIS_FIELDS=['danceability', 'mode', 'tempo', 'time_signature', 'loudness']
+MODE_MAP=["minor", "major"]
+SKIP_COMMON_WORD_COUNT=10
+TOP_TERM_COUNT=20
 
 def distance(a, b):
     dist = 0
@@ -35,11 +39,13 @@ class MusicHandler():
         for field in ANALYSIS_FIELDS:
             ti[field] = f['analysis/songs'][field][0]
 
+        # Round tempo to nearest tenths
+        ti['tempo'] = int(decimal.Decimal(int(round(ti['tempo'], -1))))
+
         ti['artist_terms'] = f['metadata/artist_terms'].value
         ti['artist_terms_freq'] = f['metadata/artist_terms_freq'].value
         ti['artist_terms_weight'] = f['metadata/artist_terms_weight'].value
 
-        # Are these the correct indexes?
         ti['artist_name'] = f['metadata/songs'][0][9]
         ti['album_name'] = f['metadata/songs'][0][14]
         ti['song_name'] = f['metadata/songs'][0][18]
@@ -51,6 +57,22 @@ class MusicHandler():
 
     # Create a new version of clusters.json
     def post_clusters(self):
+        # Res is a list of clusters.
+        # Cluster should look like
+        # {
+        #   "center": [1, 2, 3, 4],
+        #   "key_words": [top 20 words that aren't in the most common],
+        #   "tracks": ["trackids"...]
+        #   "sorted_terms": [top 20 terms by weight],
+        #   "mode_count": {"major": 32324, "minor": 234},
+        #   "median_tempo": 120,
+        #   "median"
+        # }
+        #    
+        res = {
+            "words": None,
+            "clusters": 
+
         # Load initial dataframe (df)
         df = pandas.io.parsers.read_csv(os.path.join(LOCAL_ROOT, "head1000tracks.csv"))
 
