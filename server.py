b3d27398cb20bfeefa91cd34b42485507c560f3d
Ubuntu <ubuntu@domU-12-31-39-0C-45-31.compute-1.internal>
2014-04-25 22:15:10 +0000

    starting to add stuff to server

--- server.py
+++ server.py
@@ -2,10 +2,21 @@
 
 import pandas
 import os.path
+import h5py
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from sklearn.cluster import MiniBatchKMeans, KMeans
 
-MSD_ROOT=/mnt/msd
-LOCAL_ROOT="../data"
+LOCAL_ROOT="/mnt/msd/AdditionalFiles"
+MSD_ROOT="/mnt/msd/data"
+
+def distance(a, b):
+    dist = 0
+    for i in range(len(a)):
+    	dist += (a[i] - b[i])**2
+
+    return abs(dist)
+    
+        
 
 class MusicHandler(BaseHTTPRequestHandler):
     def get_clusters():
@@ -14,10 +25,20 @@ class MusicHandler(BaseHTTPRequestHandler):
     def get_track():
         pass
 
+	def get_trackinfo(trackid):
+		ti = { "track_id": trackid }
+		myfile = os.path.join(MSD_ROOT, trackid[2], trackid[3], trackid[4], trackid + ".h5")
+		f = h5py.File(myfile, 'r')
+		
+		ti['danceability'] = f['analysis/songs']['danceability'] 
+		ti['mode'] = f['analysis/songs']['danceability'] 
+		
+		
+
     # Create a new version of clusters.json
     def post_clusters():
         # Load initial dataframe (df)
-        df = pandas.io.parsers.read_csv(os.path.join(LOCAL_ROOT, "tracks.csv"))
+        df = pandas.io.parsers.read_csv(os.path.join(LOCAL_ROOT, "head1000tracks.csv"))
 
         # Save track id for later use
         trackid = df['track_id']
@@ -30,8 +51,12 @@ class MusicHandler(BaseHTTPRequestHandler):
         df = df.fillna(0.0)
 
         # Run fit
-        kmeans = MiniBatchKMeans(n_clusters=50)
+        kmeans = MiniBatchKMeans(n_clusters=10)
         kmeans.fit(df)
+
+        # Put labels in df
+        df["_label"] = kmeans.labels_
+        df["_track_id"] = trackid
 
 
     def do_GET(self):
@@ -44,7 +69,6 @@ class MusicHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         if self.path == '/clusters.json':
             self.post_clusters()
-
 
 
 server = HTTPServer(('', 8080), MusicHandler)
