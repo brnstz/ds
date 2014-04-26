a01a6747933011da891e8a32ce06cd6638669ef2
Brian Seitz <brnstz@gmail.com>
2014-04-26 14:39:03 -0600

    fixing

--- server.py
+++ server.py
@@ -188,7 +188,7 @@ class MusicHandler():
             q.put(row)
 
         # Wait for all queues to complete
-        for i in range(len(kmeans.labels_)):
+        for i in range(kmeans.n_clusters_):
             qs[i].join()
 
         # Clean up for output
