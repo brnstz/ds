f632b1ccf6040c6846aabb7d90ba53a93906b666
Ubuntu <ubuntu@ip-10-239-22-205.ec2.internal>
2014-04-29 05:21:28 +0000

    adding doit script

---
+++ doit.sh
@@ -0,0 +1,7 @@
+./server.py 1ktracks.csv 10
+./server.py 1ktracks.csv 100
+./server.py 10ktracks.csv 100
+./server.py 50ktracks.csv 100
+./server.py 100ktracks.csv 500
+./server.py 150ktracks.csv 500
+./server.py 200ktracks.csv 500
