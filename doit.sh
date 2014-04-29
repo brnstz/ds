090ea0edb913b43f97757e641ac0d784937180c3
Ubuntu <ubuntu@ip-10-239-22-205.ec2.internal>
2014-04-29 05:33:09 +0000

    adding line

--- doit.sh
+++ doit.sh
@@ -3,5 +3,6 @@
 ./server.py 10ktracks.csv 100
 ./server.py 50ktracks.csv 100
 ./server.py 100ktracks.csv 500
+./server.py 100ktracks.csv 100
 ./server.py 150ktracks.csv 500
 ./server.py 200ktracks.csv 500
