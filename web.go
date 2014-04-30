4e6d30530c226e3393afe620b311faf2d7bedd49
Brian Seitz <brnstz@gmail.com>
2014-04-29 22:14:46 -0400

    adding spotify stuff

--- web.go
+++ web.go
@@ -6,14 +6,22 @@ import (
 	"html/template"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"path/filepath"
 )
 
 var rootDir = "/home/bseitz/proj/music"
 var jsonCt = "application/json; charset=utf-8"
 var htmlCt = "text/html; charset=utf-8"
+var spotifyUrl = "https://ws.spotify.com/search/1/track.json"
 
 type clusters []cluster
+
+type spotifyResp struct {
+	Tracks []struct {
+		Href string
+	}
+}
 
 type cluster struct {
 	Label          int     `json:"label"`
@@ -41,9 +49,21 @@ var clusterHtml = template.Must(template.New("clusterHtml").Parse(`<html>
 
         <script>
             function listenTo(artist, song, track) {
+                /*
                 console.log(artist);
                 console.log(song);
                 console.log(track);
+                */
+                var req = {
+                    "artist": artist,
+                    "song": song
+                };
+
+                $.get("/listen?" + $.param(req), null, function(data) {
+                    $("div#musicbox" + track).html(data);
+                }, "html");
+
+                return false;
             }
         </script>
     </head>
@@ -77,7 +97,7 @@ var clusterHtml = template.Must(template.New("clusterHtml").Parse(`<html>
                 <ul>
                     {{ range .Tracks }}
                     <li>{{ .ArtistName }}: {{ .SongName}} ( {{.Distance}} )
-                        <a href="javascript:listenTo('{{ .ArtistName }}', '{{ .SongName }}', '{{ .TrackId}}'); return false;">Listen!</a>
+                        <a href="javascript:listenTo('{{ .ArtistName }}', '{{ .SongName }}', '{{ .TrackId}}');>Listen!</a> <div id="musicbox{{ .TrackId}}"></div>
                     </li>
                     {{ end }}
                 </ul>
@@ -107,6 +127,7 @@ func main() {
 
 	http.HandleFunc("/cluster", func(w http.ResponseWriter, r *http.Request) {
 		//"100ktracks.csv_500"
+
 		f := filepath.Join(rootDir, fmt.Sprintf("clusters_by_distance_%v.json", r.FormValue("file")))
 		b, err := ioutil.ReadFile(f)
 
@@ -132,6 +153,51 @@ func main() {
 		w.Header().Add("Content-Type", htmlCt)
 
 		clusterHtml.Execute(w, c)
+	})
+
+	http.HandleFunc("/listen", func(w http.ResponseWriter, r *http.Request) {
+		// Get artist and song from our page
+		artist := f.FormValue("artist")
+		song := f.FormValue("song")
+
+		// Create spotify url
+		u := url.Parse(spotifyUrl)
+		q := u.Query()
+		q.Set("q", fmt.Sprintf("%v %v", artist, song))
+		u.RawQuery = q.Encode()
+
+		resp, err := http.Get(u)
+
+		// Can't run http get
+		if err != nil {
+			fmt.Fprint(w, err)
+			return
+		}
+		defer resp.Body.Close()
+
+		b, err := ioutil.ReadAll(resp.Body)
+
+		// Can't read response
+		if err != nil {
+			fmt.Fprint(w, err)
+			return
+		}
+
+		var sr spotifyResp
+		err = json.Unmarshal(b, spotifyResp)
+
+		// Can't decode json
+		if err != nil {
+			fmt.Fprint(w, err)
+			return
+		}
+
+		// No tracks
+		if len(sr.Tracks) < 1 {
+			return
+		}
+
+		fmt.Fprintf(`<iframe src="https://embed.spotify.com/?uri=%v" frameborder="0" allowtransparency="true"></iframe>`, sr.Tracks[0].Href)
 
 	})
 
