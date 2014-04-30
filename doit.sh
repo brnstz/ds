e673addeeee608c826054b4f5074daf06855d190
Brian Seitz <brnstz@gmail.com>
2014-04-30 00:38:01 -0400

    renaming server.py to cluster.py, more appropriate

--- doit.sh
+++ doit.sh
@@ -1,8 +1,8 @@
-./server.py 1ktracks.csv 10
-./server.py 1ktracks.csv 100
-./server.py 10ktracks.csv 100
-./server.py 50ktracks.csv 100
-./server.py 100ktracks.csv 500
-./server.py 100ktracks.csv 100
-./server.py 150ktracks.csv 500
-./server.py 200ktracks.csv 500
+./cluster.py 1ktracks.csv 10
+./cluster.py 1ktracks.csv 100
+./cluster.py 10ktracks.csv 100
+./cluster.py 50ktracks.csv 100
+./cluster.py 100ktracks.csv 500
+./cluster.py 100ktracks.csv 100
+./cluster.py 150ktracks.csv 500
+./cluster.py 200ktracks.csv 500
